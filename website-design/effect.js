8bdddcc20941c7ba71d8b8e23c6b20da360f2a79
HuangYuTing <s10855033@gm2.nutn.edu.tw>
2021-08-03 07:02:05 +0800

    2021/08/03

--- website-design/effect.js
+++ website-design/effect.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector("#btn");
 let sideBar = document.querySelector(".sideBar");
+let r1_btn = document.getElementsByClassName("r1_btn");
+var lastClick = r1_btn[0];
 
 btn.onclick = function(){
     sideBar.classList.toggle("active");
@@ -8,4 +10,12 @@ btn.onclick = function(){
       }else{
         btn.classList.replace("fa-times", "fa-bars");
     }
+}
+
+for (var i = 0; i < r1_btn.length; i++){
+  r1_btn[i].onclick = function(){
+    lastClick.classList.remove("selected");
+    this.classList.add("selected");
+    lastClick = this;
+  }
 }
